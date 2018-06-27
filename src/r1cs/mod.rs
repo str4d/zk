ba16645d80f3c7a1e6bf9f538086b657429ddf52
Jack Grigg <jack@z.cash>
2018-06-27 10:19:27 -0400

    impl fmt::Display for R1CS and Assignments

--- src/r1cs/mod.rs
+++ src/r1cs/mod.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use std::io;
 
 use super::ConstraintSystem;
@@ -24,13 +25,87 @@ struct Coefficient(i64);
 
 struct LinearCombination(Vec<(VariableIndex, Coefficient)>);
 
+impl LinearCombination {
+    fn fmt(&self, f: &mut fmt::Formatter, char: usize) -> Result<(), fmt::Error> {
+        if self.0.len() == 0 {
+            write!(f, "0")
+        } else {
+            let char = char as i64;
+            for (i, (v, c)) in self.0.iter().enumerate() {
+                let (negate, k) = match c {
+                    // To make the output cleaner, assume that field elements
+                    // close to the characteristic are negative. This will
+                    // mis-interpret truly-random coefficients as negative on
+                    // occasion, but it's fine for display purposes.
+                    Coefficient(k) if *k == char - 1 => (true, 1),
+                    Coefficient(k) if *k == char - 2 => (true, 2),
+                    Coefficient(k) if *k == char - 3 => (true, 3),
+                    Coefficient(k) if *k == char - 4 => (true, 4),
+                    Coefficient(k) if *k == char - 5 => (true, 5),
+                    Coefficient(k) if *k == char - 6 => (true, 6),
+                    Coefficient(k) if *k == char - 7 => (true, 7),
+                    Coefficient(k) if *k == char - 8 => (true, 8),
+                    Coefficient(k) if *k == char - 9 => (true, 9),
+                    Coefficient(k) if *k == char - 10 => (true, 10),
+                    // General cases
+                    Coefficient(k) if *k < 0 => (true, -*k),
+                    Coefficient(k) => (false, *k),
+                };
+                if negate {
+                    if i > 0 {
+                        write!(f, " - ")?
+                    } else {
+                        write!(f, "-")?
+                    }
+                } else if i > 0 {
+                    write!(f, " + ")?
+                }
+                match v {
+                    VariableIndex::Constant => write!(f, "{}", c.0)?,
+                    VariableIndex::Instance(j) => match k {
+                        1 => write!(f, "x_{}", j)?,
+                        _ => write!(f, "x_{} * {}", j, k)?,
+                    },
+                    VariableIndex::Witness(j) => match k {
+                        1 => write!(f, "w_{}", j)?,
+                        _ => write!(f, "w_{} * {}", j, k)?,
+                    },
+                }
+            }
+            Ok(())
+        }
+    }
+}
+
 struct Constraint {
     a: LinearCombination,
     b: LinearCombination,
     c: LinearCombination,
 }
 
+impl Constraint {
+    fn fmt(&self, f: &mut fmt::Formatter, char: usize) -> Result<(), fmt::Error> {
+        write!(f, "(")?;
+        self.a.fmt(f, char)?;
+        write!(f, ") * (")?;
+        self.b.fmt(f, char)?;
+        write!(f, ") = ")?;
+        self.c.fmt(f, char)
+    }
+}
+
 struct Assignment(VariableIndex, i64);
+
+impl fmt::Display for Assignment {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        match self.0 {
+            VariableIndex::Constant => write!(f, "Constant = {}", self.1)?,
+            VariableIndex::Instance(i) => write!(f, "x_{} = {}", i, self.1)?,
+            VariableIndex::Witness(i) => write!(f, "w_{} = {}", i, self.1)?,
+        }
+        Ok(())
+    }
+}
 
 struct Header {
     v: usize,
@@ -83,6 +158,23 @@ impl ConstraintSystem for R1CS {
     }
 }
 
+impl fmt::Display for R1CS {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, "Version:           {}\n", self.0.v)?;
+        write!(f, "Characteristic:    {}\n", self.0.p)?;
+        write!(f, "Degree:            {}\n", self.0.m)?;
+        write!(f, "Input variables:   {}\n", self.0.nx)?;
+        write!(f, "Witness variables: {}\n", self.0.nw)?;
+        write!(f, "Constraints:\n")?;
+        for c in &self.1 {
+            write!(f, "  ")?;
+            c.fmt(f, self.0.p)?;
+            write!(f, "\n")?;
+        }
+        Ok(())
+    }
+}
+
 pub struct Assignments(Header, Vec<Assignment>);
 
 impl Assignments {
@@ -99,5 +191,20 @@ impl Assignments {
     fn encode(&self) -> Vec<u8> {
         // TODO
         Vec::new()
+    }
+}
+
+impl fmt::Display for Assignments {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, "Version:           {}\n", self.0.v)?;
+        write!(f, "Characteristic:    {}\n", self.0.p)?;
+        write!(f, "Degree:            {}\n", self.0.m)?;
+        write!(f, "Input variables:   {}\n", self.0.nx)?;
+        write!(f, "Witness variables: {}\n", self.0.nw)?;
+        write!(f, "Assignments:\n")?;
+        for a in &self.1 {
+            write!(f, "  {}\n", a)?;
+        }
+        Ok(())
     }
 }
